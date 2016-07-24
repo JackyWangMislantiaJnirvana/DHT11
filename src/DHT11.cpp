70062b30b51f25f475db2a6d89f0066988365f4a
JackyWangMislantiaJnirvana <wmjwld@live.cn>
2016-07-24 22:45:14 +0800

    [Development] Finished most part of getData()
    [  Finished  ] Connection openning
    [  Finished  ] Data recieving
    [  Finished  ] Data verify
    [ On Working ] Temprature and Humidity data convert

--- src/DHT11.cpp
+++ src/DHT11.cpp
@@ -1,11 +1,12 @@
-#include <DHT11.h>
+#include <math.h>
+#include "DHT11.h"
 
 #define BUS_HIGH LOW
 #define BUS_LOW HIGH
 
 DHT11::DHT11(int dhtPin) 
 {
-	this -> dhtPin = dhtPin;	// Make the pin number can be accessed wherever in the class
+	this -> dhtPin = dhtPin;							// Make the pin number can be accessed wherever in the class
 }
 
 
@@ -13,8 +14,13 @@ int DHT11::getData(void)
 {
 	/* Prepare */
 	int dhtPin = this -> dhtPin;
-	byte buffer[40] = {};		// Buffer for the data that recieved from DHT11
-	int waitTime = 0;		// NOTE: time in MICROSECONDS
+	int buffer[40] = {0};								// Buffer for the data that recieved from DHT11
+	unsigned long waitTime = 0;							// NOTE: time in MICROSECONDS
+	unsigned long signalLen = 0;
+	int bitCount = 0;
+	int checksum = 0;
+	long int receivedChecksum = 0;
+	long int caculatedChecksum = 0;
 
 	/* Wake up DHT11 */
 	pinMode(dhtPin, OUTPUT);
@@ -22,25 +28,90 @@ int DHT11::getData(void)
 	delay(20);
 	digitalWrite(dhtPin, BUS_LOW);
 
-	/* Wait for DHT11's reply */
+	/* Begin to listen from DHT11 */
 	pinMode(dhtPin, INPUT);
-	waitTime = 0;			// Reset the timer
-	while ( digitalRead(dhtPin) == BUS_LOW )
-	{
-		waitTime ++;		// Increase the timer
 
-		if waitTime >= 1000
-			return -1;	// Connection time out
+	/* Wait for DHT11's reply */
+	waitTime = micros();								// Reset the timer
+	while (digitalRead(dhtPin) == BUS_LOW)
+	{
+		if (micros() - waitTime >= 1000)
+			delay(100);
+			return -1;									// Connection time out, give up the connection
+	}
+
+	/* Wait until finishing receiving the 80us reply signal */
+	waitTime = micros();								// Reset the timer
+	while ( digitalRead(dhtPin) == BUS_HIGH )
+	{
+		if (micros() - waitTime >= 1000)
+			delay(100);
+			return -2;									// Connection time out, give up the connection
 	}
 	
-	/* Recieve the 80us confirn signal */
-	while ()
-	
+	/* Receive data, 1 bit each loop */
+	for (int bitCount = 0; bitCount < 40;)
+	{
+		/* Wait through the 50us bit signal */
+		while (digitalRead(dhtPin) == BUS_HIGH)
+		{
+			// Empty block
+		}
 
+		/* Receive and  storage the data */
+			/* Get the length of the signal */
+		waitTime = micros();
+		while (digitalRead(dhtPin) == BUS_LOW)
+		{
+			// Empty block
+		}
+		signalLen = micros() - waitTime;
+			/* Tell the signal means 1 or 0 */
+		if (signalLen > 20 && signalLen < 50)
+		{
+			buffer[bitCount] = 1;						// 1
+			bitCount ++;
+		}
+		if (signalLen > 50 && signalLen < 80)
+		{
+			buffer[bitCount] = 0;						// 0
+			bitCount ++;
+		}
+	}
 
+	/* Use the checksum to verify if the data received is correct */
+		/* Convert checksum that received from DHT11 */
+	for (int i = 0; i < 8; i ++)
+	{
+		receivedChecksum += buffer[39-i]*pow(2, i);
+	}
+
+		/* Calculate checksum according to the data received */
+	int tempSum = 0;									// Variable for temporary usage
+	for (int i = 0; i < 39; i ++)
+	{
+		for (int j = 0; j < 8; j ++)
+		{
+			tempSum += buffer[i] * pow(2, j);
+		}
+	}
+	caculatedChecksum = tempSum % 256;					// Cut off the number higher than 8 bit
+		/* Verify */
+	if 	(caculatedChecksum != receivedChecksum)
+	{
+		return -3;										// Data incorrect
+	}
+
+	// To-Do: temperature and humidity data convert
 
 }
 
-int DHT11::getTemp(void) {}
+int DHT11::getTemp(void)
+{
+	// To-Do: temperature data calculation
+}
 
-int DHT11::getHumi(void) {}
+int DHT11::getHumi(void)
+{
+	// To-Do: humidity data calculation
+}
